c98b345164300fcf89eb714e1738604694987e1c
kseniia <ofmybest@gmail.com>
2022-06-26 21:48:10 +0200

    Redirect

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.get("https://playground.learnqa.ru/api/long_redirect")
+first_response = response.history[0]
+second_response = response.history[1]
+fird_response = response
+
+print(len(response.history))
+print(first_response.url)
+print(second_response.url)
+print(fird_response.url)
+
+
+
