f43cd0a4c8d41fdac47442634905937a9304fb97
kseniia <ofmybest@gmail.com>
2022-06-27 10:31:44 +0200

    Redirect2

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-first_response = response.history[0]
-second_response = response.history[1]
-fird_response = response
+end_response = response
 
 print(len(response.history))
-print(first_response.url)
-print(second_response.url)
-print(fird_response.url)
+print(end_response.url)
 
 
 
