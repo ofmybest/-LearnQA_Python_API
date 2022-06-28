a92a6ce66aefb968b65be4ae6b3a2f351d9d693d
kseniia <ofmybest@gmail.com>
2022-06-28 20:56:22 +0200

    Query type

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-end_response = response
+response = requests.put("https://playground.learnqa.ru/api/compare_query_type")
+print(response.text)
 
-print(len(response.history))
-print(end_response.url)
+response = requests.head("https://playground.learnqa.ru/api/compare_query_type", data={"method": "HEAD"})
+print(response.text)
 
+response = requests.put("https://playground.learnqa.ru/api/compare_query_type", data={"method": "PUT"})
+print(response.text)
 
+methods = ["GET","PUT","POST","DELETE"]
+for method in methods:
+    response = requests.get("https://playground.learnqa.ru/api/compare_query_type", params={"method": method})
+    print("get - " + method)
+    print(response.status_code)
+    print(response.text)
+    print("---------")
 
+    response = requests.put("https://playground.learnqa.ru/api/compare_query_type", data={"method": method})
+    print("put - " + method)
+    print(response.status_code)
+    print(response.text)
+    print("---------")
+
+    response = requests.post("https://playground.learnqa.ru/api/compare_query_type", data={"method": method})
+    print("post - " + method)
+    print(response.status_code)
+    print(response.text)
+    print("---------")
+
+    response = requests.delete("https://playground.learnqa.ru/api/compare_query_type", data={"method": method})
+    print("delete - " + method)
+    print(response.status_code)
+    print(response.text)
+    print("---------")
