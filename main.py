735fca4a0e4bdb40c5cddf4d57e8bb0f81c60dd3
kseniia <ofmybest@gmail.com>
2022-06-30 21:23:05 +0200

    Longtime_job

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
+import time
 import requests
+import json
 
-response = requests.put("https://playground.learnqa.ru/api/compare_query_type")
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
+obj = json.loads(response.text)
 print(response.text)
 
-response = requests.head("https://playground.learnqa.ru/api/compare_query_type", data={"method": "HEAD"})
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": obj['token']})
 print(response.text)
 
-response = requests.put("https://playground.learnqa.ru/api/compare_query_type", data={"method": "PUT"})
+time.sleep(obj['seconds'])
+
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": obj['token']})
 print(response.text)
-
-methods = ["GET","PUT","POST","DELETE"]
-for method in methods:
-    response = requests.get("https://playground.learnqa.ru/api/compare_query_type", params={"method": method})
-    print("get - " + method)
-    print(response.status_code)
-    print(response.text)
-    print("---------")
-
-    response = requests.put("https://playground.learnqa.ru/api/compare_query_type", data={"method": method})
-    print("put - " + method)
-    print(response.status_code)
-    print(response.text)
-    print("---------")
-
-    response = requests.post("https://playground.learnqa.ru/api/compare_query_type", data={"method": method})
-    print("post - " + method)
-    print(response.status_code)
-    print(response.text)
-    print("---------")
-
-    response = requests.delete("https://playground.learnqa.ru/api/compare_query_type", data={"method": method})
-    print("delete - " + method)
-    print(response.status_code)
-    print(response.text)
-    print("---------")
